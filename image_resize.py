9becae50eed31163e20cea98de6e3cdf28103ac9
Nitin Dhiman <34596466+NitinD97@users.noreply.github.com>
2020-01-14 12:55:07 +0530

    added resize code OpenCV

---
+++ image_resize.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+
+
+def resize(image_name, x=100, y=100):
+    # flag = 1 for colored, 0 for black and white, -1 color with alpha (transparency)
+    img = cv2.imread(image_name, 0)
+    image_name = os.path.split(image_name)
+    new_name = 'resized_'+image_name[-1]
+    new_name = os.path.join(*image_name[:-1], new_name)
+    resized_image = cv2.resize(img, (x, y))
+    cv2.imwrite(new_name, resized_image)
+
+
+def try_multiple_image_resizing():
+    dir_name = 'path/to/images/directory'
+    for image in os.listdir(dir_name):
+        if not image[-4:] == '.jpg' and not image[-4:] == '.png':
+            continue
+        image_name = os.path.join(dir_name, image)
+        # change size params
+        width, height = 1900, 1080
+        resize(image_name, width, height)
+
+
+if __name__ == '__main__':
+    try_multiple_image_resizing()
